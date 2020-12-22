a0f0d0e5542281786a307a2d28cfc31543c101f5
longwatermelon <qianhansenb@gmail.com>
2020-12-22 15:05:40 -0700

    idk if this works i didnt test it

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)       {
+    for (int i = 0; i < 2147483646; i++)`
+        if ( i == number  ) { System.out.println("The number Is Even!"); }
+    
+    for (int i=0; i > -2147483646, --i)
+    {
+
+    if (i == number ) { System.out.println("The number is Even!");}}
+ 
+}}
